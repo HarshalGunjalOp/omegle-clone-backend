2cf7321b8e7981340bb83bb5e62737628c9ecac2
hrshlgunjal3@gmail.com <hrshlgunjal3@gmail.com>
2025-01-28 13:04:32 +0000

    fixed a bug

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,7 @@ io.on('connection', (socket: Socket) => {
   })
 });
 
-const PORT = process.env.PORT || 443;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
